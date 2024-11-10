d8268861abe2f60b9f3b5e2968bacddf770d6fac
Jasmit <138936262+Jasmit22@users.noreply.github.com>
2024-11-09 20:34:59 -0700

    dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -28,13 +28,23 @@ export default function DashboardPage() {
     fetchData();
   }, []);
 
+  // Helper function to truncate text to 50 words
+  const truncateText = (text, wordLimit = 50) => {
+    const words = text.split(" ");
+    if (words.length > wordLimit) {
+      return words.slice(0, wordLimit).join(" ") + "...";
+    }
+    return text;
+  };
+
   // Smooth scroll handler to scroll to the specified section with offset
   const onScrollToSection = (continent) => {
     const targetSection = document.getElementById(continent);
     const headerOffset = 80; // Adjust this value based on your sticky header height
 
     if (targetSection) {
-      const elementPosition = targetSection.getBoundingClientRect().top + window.scrollY;
+      const elementPosition =
+        targetSection.getBoundingClientRect().top + window.scrollY;
       const offsetPosition = elementPosition - headerOffset;
 
       window.scrollTo({
@@ -53,7 +63,15 @@ export default function DashboardPage() {
   }
 
   // Group conflicts by continent
-  const continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"];
+  const continents = [
+    "Africa",
+    "Asia",
+    "Europe",
+    "North America",
+    "South America",
+    "Oceania",
+    "Antarctica",
+  ];
   const conflictsByContinent = continents.map((continent) => ({
     continent,
     conflicts: conflicts.filter((conflict) => conflict.continent === continent),
@@ -74,63 +92,55 @@ export default function DashboardPage() {
         </h2>
 
         {/* Display conflicts grouped by continent */}
-        {conflictsByContinent.map(({ continent, conflicts }) => (
-          conflicts.length > 0 && ( // Only show section if there are conflicts for the continent
-            <div key={continent} className="mb-12" id={continent}>
-              <hr className="border-example-third_colour mb-4" />
-              <h3 className="text-xl font-semibold text-foreground mb-4 text-center">
-                {continent}
-              </h3>
-              <hr className="border-example-third_colour mb-6" />
+        {conflictsByContinent.map(
+          ({ continent, conflicts }) =>
+            conflicts.length > 0 && ( // Only show section if there are conflicts for the continent
+              <div key={continent} className="mb-12" id={continent}>
+                <hr className="border-example-third_colour mb-4" />
+                <h3 className="text-xl font-semibold text-foreground mb-4 text-center">
+                  {continent}
+                </h3>
+                <hr className="border-example-third_colour mb-6" />
 
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {conflicts.map((conflict) => (
-                  <div
-                    key={conflict.id}
-                    onClick={() => router.push(`/chat/${conflict.id}`)}
-                    className="rounded-lg shadow-lg hover:shadow-xl p-4 bg-background border border-example-third_colour transition-transform transform hover:scale-105 cursor-pointer"
-                  >
-                    <img
-                      src={conflict.imageSrcUrl}
-                      alt={conflict.title}
-                      className="w-full h-48 object-cover rounded-md mb-4"
-                    />
-                    <h3 className="text-lg sm:text-xl font-semibold text-foreground mb-2">
-                      {conflict.title}
-                    </h3>
-                    <p className="text-sm sm:text-base text-foreground mb-2">
-                      {conflict.description}
-                    </p>
-                    <p className="text-xs sm:text-sm text-foreground mb-1">
-                      <strong className="text-example-third_colour">Casualties:</strong> {conflict.casualties}
-                    </p>
-                    <p className="text-xs sm:text-sm text-foreground mb-1">
-                      <strong className="text-example-third_colour">Displacement:</strong> {conflict.displacement}
-                    </p>
-                    <p className="text-xs sm:text-sm text-foreground mt-2">
-                      {conflict.basic_info}
-                    </p>
-                    <h4 className="mt-4 font-semibold text-example-third_colour">Resources:</h4>
-                    <ul className="list-disc list-inside text-xs sm:text-sm text-example-third_colour">
-                      {conflict.charities_resources.map((resource, index) => (
-                        <li key={index}>
-                          <a
-                            href={resource.url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="hover:underline"
-                          >
-                            {resource.name}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                ))}
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                  {conflicts.map((conflict) => (
+                    <div
+                      key={conflict.id}
+                      onClick={() => router.push(`/chat/${conflict.id}`)}
+                      className="rounded-lg shadow-lg hover:shadow-xl p-4 bg-background border border-example-third_colour transition-transform transform hover:scale-105 cursor-pointer"
+                    >
+                      <img
+                        src={conflict.imageSrcUrl}
+                        alt={conflict.title}
+                        className="w-full h-48 object-cover rounded-md mb-4"
+                      />
+                      <h3 className="text-lg sm:text-xl font-semibold text-foreground mb-2">
+                        {conflict.title}
+                      </h3>
+                      <p className="text-sm sm:text-base text-foreground mb-2">
+                        {truncateText(conflict.description)}
+                      </p>
+                      <p className="text-xs sm:text-sm text-foreground mb-1">
+                        <strong className="text-example-third_colour">
+                          Casualties:
+                        </strong>{" "}
+                        {conflict.casualties}
+                      </p>
+                      <p className="text-xs sm:text-sm text-foreground mb-1">
+                        <strong className="text-example-third_colour">
+                          Displacement:
+                        </strong>{" "}
+                        {conflict.displacement}
+                      </p>
+                      <p className="text-xs sm:text-sm text-foreground mt-2">
+                        {conflict.basic_info}
+                      </p>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          )
-        ))}
+            )
+        )}
       </div>
       <Footer />
     </div>
